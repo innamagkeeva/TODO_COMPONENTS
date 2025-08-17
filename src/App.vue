82b4fb08430476633da4616abdcf488aae961038
Inna Magkeeva <inna.magkeeva79@gmail.com>
2025-08-18 00:44:37 +0300

    половина верстки

---
+++ src/App.vue
@@ -0,0 +1,155 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const inputTaskTextHigh = ref('')
+const inputTaskTextLow = ref('')
+
+interface Task {
+  text: string
+  status: 'done' | 'notDone'
+  priority: 'high' | 'low'
+}
+
+const tasks = ref<Task[]>([])
+
+function addTask(): void {
+  const textHigh = inputTaskTextHigh.value.trim()
+  const textLow = inputTaskTextLow.value.trim()
+  if (textHigh) {
+    const capitalizedHighText = textHigh[0].toUpperCase() + textHigh.slice(1)
+    tasks.value.push({ text: capitalizedHighText, status: 'notDone', priority: 'high' })
+    inputTaskTextHigh.value = ''
+    console.log('сработало')
+  }
+  if (textLow) {
+    const capitalizedLowText = textLow[0].toUpperCase() + textLow.slice(1)
+    tasks.value.push({ text: capitalizedLowText, status: 'notDone', priority: 'low' })
+    inputTaskTextLow.value = ''
+  }
+}
+
+const highPriorityTasks = computed(() => {
+  return tasks.value.filter((task) => task.priority === 'high')
+})
+
+const lowPriorityTasks = computed(() => {
+  return tasks.value.filter((task) => task.priority === 'low')
+})
+</script>
+
+<template>
+  <div class="tasks">
+    <h1 class="title">HIGH</h1>
+    <form
+      class="form"
+      @submit.prevent="addTask"
+    >
+      <input
+        class="form__input"
+        type="text"
+        name="task"
+        placeholder="Добавить задачу"
+        v-model="inputTaskTextHigh"
+      />
+      <button
+        class="Form__button"
+        type="submit"
+      >
+        +
+      </button>
+    </form>
+    <ul class="list">
+      <li
+        class="list__item"
+        v-for="task in highPriorityTasks"
+        :key="task.text"
+      >
+        <input
+          class="list__checkbox"
+          type="checkbox"
+        />
+        <p class="list__text">{{ task.text }}</p>
+        <button class="list__btn">+</button>
+      </li>
+    </ul>
+    <h1 class="title">LOW</h1>
+    <form
+      class="form"
+      @submit.prevent="addTask"
+    >
+      <input
+        class="form__input"
+        type="text"
+        placeholder="Добавить задачу"
+        v-model="inputTaskTextLow"
+      />
+      <button class="Form__button">+</button>
+    </form>
+    <ul class="list">
+      <li
+        class="list__item"
+        v-for="task in lowPriorityTasks"
+        :key="task.text"
+      >
+        <input
+          class="list__checkbox"
+          type="checkbox"
+        />
+        <p class="list__text">{{ task.text }}</p>
+        <button class="list__btn">+</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.tasks {
+  width: 500px;
+  height: 650px;
+  border: 2px solid black;
+  margin: 0 auto;
+  padding: 10px 20px;
+  font-size: 16px;
+}
+
+.title {
+  width: 100px;
+  margin: 0 auto;
+  margin-bottom: 20px;
+}
+
+.form,
+.list__item {
+  max-width: 100%;
+  height: 45px;
+  border: 1px solid black;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px;
+}
+
+.list {
+  padding: 0;
+  margin: 0;
+}
+
+.list__btn {
+  rotate: 45deg;
+}
+
+button {
+  border: none;
+  background-color: white;
+  cursor: pointer;
+}
+
+.list__text {
+  display: flex;
+  flex-grow: 1;
+  align-items: center;
+  height: 30px;
+}
+</style>
